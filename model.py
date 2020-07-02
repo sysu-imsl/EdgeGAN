0b52fe16219f274ac99cac4005c6f94fba74000e
liuQ <1552063273@qq.com>
2020-07-02 21:59:33 +0800

    dive into patch2 to find bug

--- model.py
+++ model.py
@@ -749,8 +749,13 @@ class DCGAN(object):
                 grad_l2 = tf.sqrt(tf.reduce_sum(
                     tf.square(gradients), axis=[1, 2, 3]))
                 gradient_penalty = tf.reduce_mean((grad_l2 - 1) ** 2)
+                grad_pen = self.config.lambda_gp * gradient_penalty
+                self.d_loss_patch2 = save_layer(
+                    'image_d_loss_origin', self.d_loss_patch2)
+                grad_pen = save_layer(
+                    'image_d_loss_grad_penalty_origin', grad_pen)
 
-                self.d_loss_patch2 += self.config.lambda_gp * gradient_penalty
+                self.d_loss_patch2 += grad_pen
                 self.d_loss_patch2 = save_layer(
                     'd_loss_patch2_origin', self.d_loss_patch2)
                 self.g_loss_patch2 = tf.reduce_mean(self.patch2_D_logits_ * -1)
